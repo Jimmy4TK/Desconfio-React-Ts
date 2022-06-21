dee639661e3e948668540404f5261c58c694f3da
Jimmy4TK <juanfalco@hotmail.com.ar>
2022-06-21 00:32:23 -0300

    Add Login, register and change password

---
+++ src/app/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import Welcome from "../welcome/Welcome";
+import LoginMenu from '../user/LoginMenu';
+import NavBarMenu from "../menu/Navbar";
+import Info from '../info/Info'
+import Password from '../user/Password';
+import { StateLoggedInRoute } from '../common/components/LoggedInRoute';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+              <NavBarMenu />
+              <Routes>    
+                <Route path="/" element={<Welcome />} />
+                <Route path="/login" element={<LoginMenu />} />
+                <Route path="/register" element={<LoginMenu />} />
+                <Route path="/info" element={<StateLoggedInRoute component={Info} />} />
+                <Route path="/password" element={<StateLoggedInRoute component={Password} />} />
+              </Routes>
+      <Outlet />
+    </BrowserRouter >
+  );
+}
+
---
+++ src/user/Password.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import DangerLabel from "../common/components/DangerLabel"
+import Form from "../common/components/Form"
+import FormAcceptButton from "../common/components/FormAcceptButton"
+import FormButton from "../common/components/FormButton"
+import FormButtonBar from "../common/components/FormButtonBar"
+import { Container } from "react-bootstrap"
+import FormPassword from "../common/components/FormPassword"
+import FormTitle from "../common/components/FormTitle"
+import GlobalContent from "../common/components/GlobalContent"
+import { useErrorHandler } from "../common/utils/ErrorHandler"
+import { changePassword, User } from "./userService"
+import { useSessionUser } from "../store/userStore"
+
+
+export default function Password() {
+    const history = useNavigate()
+    const [password, setPassword] = useState("")
+    const [new_password, setNewPassword] = useState("")
+    const [confirm_password, setConfirm_Password] = useState("")
+    const user:User|undefined = useSessionUser()
+
+    const errorHandler = useErrorHandler()
+
+    const updatePasswordClick = async () => {
+        errorHandler.cleanRestValidations()
+
+        if (!password) {
+            errorHandler.addError("password", "No puede estar vacío")
+        }
+        if (!new_password) {
+            errorHandler.addError("new_password", "No puede estar vacío")
+        }
+        if (new_password !== confirm_password) {
+            errorHandler.addError("confirm_password", "Las contraseñas no coinciden")
+        }
+
+        if (errorHandler.hasErrors()) {
+            return
+        }
+
+        try {
+            await changePassword({
+                password,
+                new_password,
+                confirm_password,
+                id: user!.id
+            })
+            history("/")
+        } catch (error) {
+            errorHandler.processRestValidations(error)
+        }
+    }
+
+    return (
+        <Container className="mt-2">
+            <GlobalContent>
+                <FormTitle>Cambiar Password</FormTitle>
+
+                <Form>
+                    <div className="col-5"> 
+                        <FormPassword
+                            label="Password Actual"
+                            name="password"
+                            errorHandler={errorHandler}
+                            onChange={event => setPassword(event.target.value)} />
+
+                        <FormPassword
+                            label="Nuevo Password"
+                            name="new_password"
+                            errorHandler={errorHandler}
+                            onChange={event => setNewPassword(event.target.value)} />
+
+                        <FormPassword
+                            label="Repetir Password"
+                            name="confirm_password"
+                            errorHandler={errorHandler}
+                            onChange={event => setConfirm_Password(event.target.value)} />
+
+                        <DangerLabel message={errorHandler.errorMessage} />
+
+                        <FormButtonBar>
+                            <FormAcceptButton label="Cambiar" onClick={updatePasswordClick} />
+                            <FormButton label="Cancelar" onClick={() => history("/")} />
+                        </FormButtonBar>
+                    </div>
+                </Form >
+            </GlobalContent>
+        </Container>
+    )
+}
